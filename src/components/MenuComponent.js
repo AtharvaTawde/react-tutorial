a02c120300579d36217570ad2c305d225838fbde
AtharvaTawde <atharvatawde894@gmail.com>
2021-04-07 13:16:35 -0700

    Header and Footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -13,13 +13,11 @@ function RenderMenuItem({ dish, onClick }) {
 }
 
 function Menu(props) {
-	const menu = props.dishes.map((dish) => {
-		return (
-			<div key={dish.id} className="col-12 col-md-5 m-1">
-				<RenderMenuItem dish={dish} onClick={props.onClick} />
-			</div>
-		);
-	});
+	const menu = props.dishes.map((dish) =>
+		<div key={dish.id} className="col-12 col-md-5 m-1">
+			<RenderMenuItem dish={dish} onClick={props.onClick} />
+		</div>
+	);
 
 	return (
 		<div className="container">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
