a19b5ddfaf50e11b28db9fe30374c7b108244807
AtharvaTawde <atharvatawde894@gmail.com>
2021-04-08 15:25:55 -0700

    Controlled Form Validation

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 class Contact extends Component {
-	
+
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -14,9 +14,16 @@ class Contact extends Component {
 			message: '',
 			agree: false,
 			contactType: 'Tel.',
+			touched: {
+				firstname: false,
+				lastname: false,
+				telnum: false,
+				email: false,
+			},
 		};
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleBlur = this.handleBlur.bind(this);
 	}
 
 	handleInputChange(event) {
@@ -34,8 +41,47 @@ class Contact extends Component {
 		alert(JSON.stringify(this.state));
 		event.preventDefault(); // prevents page from refreshing
 	}
-	
+
+	handleBlur = (field) => (evt) => {
+		this.setState({
+			touched: { ...this.state.touched, [field]: true }
+		});
+	}
+
+	validate(firstname, lastname, telnum, email) {
+		const errors = {
+			firstname: '',
+			lastname: '',
+			telnum: '',
+			email: '',
+		}
+
+		if (this.state.touched.firstname && firstname.length < 3) {
+			errors.firstname = 'First Name should be >= 3 characters';
+		} else if (this.state.touched.firstname && firstname.length > 10) {
+			errors.firstname = 'First Name should be <= 10 characters';
+		}
+
+		if (this.state.touched.lastname && lastname.length < 3) {
+			errors.lastname = 'Last Name should be >= 3 characters';
+		} else if (this.state.touched.lastname && lastname.length > 10) {
+			errors.lastname = 'Last Name should be <= 10 characters';
+		}
+
+		const reg = /^\d+$/;
+		if (this.state.touched.telnum && !reg.test(telnum)) {
+			errors.telnum = "Tel. Number should contain only numbers.";
+		}
+
+		if (this.state.touched.email && email.split('').filter(x => x === '@').length !== 1) {
+			errors.email = "Email should contain an @ sign.";
+		}
+
+		return errors;
+	}
+
 	render() {
+		const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
 		return (
 			<div className="container" >
 				<div className="row">
@@ -83,36 +129,63 @@ class Contact extends Component {
 							<FormGroup row>
 								<Label htmlFor="firstname" md={2}>First Name</Label>
 								<Col md={10}>
-									<Input type="text" id="firstname" name="firstname" placeholder="First Name" value={this.state.firstname} onChange={this.handleInputChange} />
+									<Input type="text" id="firstname" name="firstname" placeholder="First Name"
+										value={this.state.firstname}
+										onChange={this.handleInputChange}
+										valid={errors.firstname === ''}
+										invalid={errors.firstname !== ''}
+										onBlur={this.handleBlur('firstname')}
+									/>
+									<FormFeedback>{errors.firstname}</FormFeedback>
 								</Col>
 							</FormGroup>
 							<FormGroup row>
 								<Label htmlFor="lastname" md={2}>Last Name</Label>
 								<Col md={10}>
-									<Input type="text" id="lastname" name="lastname" placeholder="Last Name" value={this.state.lastname} onChange={this.handleInputChange} />
+									<Input type="text" id="lastname" name="lastname" placeholder="Last Name"
+										value={this.state.lastname}
+										onChange={this.handleInputChange}
+										valid={errors.lastname === ''}
+										invalid={errors.lastname !== ''}
+										onBlur={this.handleBlur('lastname')} />
+									<FormFeedback>{errors.lastname}</FormFeedback>
 								</Col>
 							</FormGroup>
 							<FormGroup row>
 								<Label htmlFor="telnum" md={2}>Contact Tel.</Label>
 								<Col md={10}>
-									<Input type="tel" id="telnum" name="telnum" placeholder="Tel. Number" value={this.state.telnum} onChange={this.handleInputChange} />
+									<Input type="tel" id="telnum" name="telnum" placeholder="Tel. Number"
+										value={this.state.telnum}
+										onChange={this.handleInputChange}
+										valid={errors.telnum === ''}
+										invalid={errors.telnum !== ''}
+										onBlur={this.handleBlur('telnum')}
+									/>
+									<FormFeedback>{errors.telnum}</FormFeedback>
 								</Col>
 							</FormGroup>
 							<FormGroup row>
 								<Label htmlFor="email" md={2}>Email</Label>
 								<Col md={10}>
-									<Input type="email" id="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleInputChange} />
+									<Input type="email" id="email" name="email" placeholder="Email"
+										value={this.state.email}
+										onChange={this.handleInputChange}
+										valid={errors.email === ''}
+										invalid={errors.email !== ''}
+										onBlur={this.handleBlur('email')}
+									/>
+									<FormFeedback>{errors.email}</FormFeedback>
 								</Col>
 							</FormGroup>
 							<FormGroup row>
-								<Col md={{size: 6, offset: 2}}>
+								<Col md={{ size: 6, offset: 2 }}>
 									<FormGroup check>
 										<Label check>
 											<Input type="checkbox" name="agree" checked={this.state.agree} onChange={this.handleInputChange} /> {' '} <strong>May we contact you?</strong>
 										</Label>
 									</FormGroup>
 								</Col>
-								<Col md={{size: 3, offset: 1}}>
+								<Col md={{ size: 3, offset: 1 }}>
 									<Input type="select" name="contactType" value={this.state.contactType} onChange={this.handleInputChange} >
 										<option>Tel.</option>
 										<option>Email</option>
@@ -126,7 +199,7 @@ class Contact extends Component {
 								</Col>
 							</FormGroup>
 							<FormGroup row>
-								<Col md={{size: 10, offset: 2}}>
+								<Col md={{ size: 10, offset: 2 }}>
 									<Button type="submit" color="primary">Send Feedback</Button>
 								</Col>
 							</FormGroup>
