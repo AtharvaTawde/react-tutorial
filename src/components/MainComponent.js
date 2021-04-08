130171e8b0cbd03bcf2835b467f45cb0e843085d
AtharvaTawde <atharvatawde894@gmail.com>
2021-04-07 17:12:51 -0700

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Redirect, Route, Switch } from 'react-router';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -36,9 +37,9 @@ class Main extends Component {
 
 		const DishWithId = ({ match }) => {
 			return (
-				<DishDetail 
-					dish={this.state.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]} 
-					comments={this.state.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}	
+				<DishDetail
+					dish={this.state.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}
+					comments={this.state.comments.filter(comment => comment.dishId === parseInt(match.params.dishId, 10))}
 				/>
 			);
 		}
@@ -51,6 +52,7 @@ class Main extends Component {
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
 					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
+					<Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
