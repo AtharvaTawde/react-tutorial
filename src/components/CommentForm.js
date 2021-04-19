d52fd1edce8a00c5f9c87a171e5f46e049e8827c
AtharvaTawde <atharvatawde894@gmail.com>
2021-04-19 09:10:12 -0700

    Combining Reducers

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,12 +1,4 @@
-import React, { Component, useState } from "react";
 import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Breadcrumb,
-  BreadcrumbItem,
   Button,
   Col,
   Label,
@@ -15,54 +7,11 @@ import {
   ModalHeader,
   Row,
 } from "reactstrap";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
 const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
-
-function RenderDish({ dish }) {
-  if (dish != null) {
-    return (
-      <Card>
-        <CardImg width="100%" object src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
-    );
-  } else {
-    return <div></div>;
-  }
-}
-
-function RenderComments({ comments }) {
-  if (comments != null) {
-    const commentBlock = comments.map((comment) => (
-      <li key={comment.id}>
-        <p>{comment.comment}</p>
-        <p>
-          --{comment.author},{" "}
-          {new Intl.DateTimeFormat("en-US", {
-            year: "numeric",
-            month: "short",
-            day: "2-digit",
-          }).format(new Date(Date.parse(comment.date)))}
-        </p>
-      </li>
-    ));
-
-    return (
-      <div>
-        <h4>Comments</h4>
-        <ul className="list-unstyled">{commentBlock}</ul>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
-}
 
 class CommentForm extends Component {
   constructor(props) {
@@ -158,37 +107,4 @@ class CommentForm extends Component {
   }
 }
 
-function DishDetail(props) {
-  if (props.dish != null) {
-    console.log(props.comments);
-    return (
-      <div className="container">
-        <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/menu">Menu</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>{props.dish.name}</h3>
-            <hr />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish} />
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments} />
-            <CommentForm />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
-}
-
-export default DishDetail;
+export default CommentForm;
